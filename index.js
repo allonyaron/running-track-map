ac1db488aeedd418e3c06f90c2bde7968d35eb2f
ayaron <allonyaron@gmail.com>
2015-07-21 20:48:52 +0000

    remove hardcoded coordinated

--- index.js
+++ index.js
@@ -4,39 +4,14 @@ var exphbs  = require('express-handlebars');
 var MongoClient = require('mongodb').MongoClient;  
 
 var app = express();
-var runningTracks = {};
-var runningTrackArr = [];
 
 app.engine('hbs', exphbs({extname:'hbs', defaultLayout:'main.hbs'}));
 app.set('view engine', 'hbs');
 
 app.use(express.static('public'));
 
-//get running track data 
-MongoClient.connect('mongodb://localhost:27017/running', function(err, db) {
-    
-    if(err) throw err;
-    db.collection('runningtracks').find({geometry : {$near : {$geometry : { type : "Point" , coordinates : [-73.98267659999999, 40.6758102] }}}}).toArray(function(err, docs) {
-//    db.collection('runningtracks').find().toArray(function(err, docs) {
- 
-        if(err) throw err;
-            docs.forEach(function(doc) {
-                
-                     var track = {
-                        name : doc.name,
-                        lat : doc.geometry.coordinates[1],
-                        lng : doc.geometry.coordinates[0]
-                     };
-                //console.log(track);     
-                    runningTrackArr.push(track);
-            });
-        runningTracks.runningTrackArr = runningTrackArr;
-    });
-});
- 
+//get nearest running track api
 app.get('/api/nearby/:lng/:lat',function(req,res) {
-    
-   // res.send(req.params.lng);
     
     MongoClient.connect('mongodb://localhost:27017/running', function(err, db) {
         if(err) console.log('connection error');
@@ -45,9 +20,7 @@ app.get('/api/nearby/:lng/:lat',function(req,res) {
             if(err) throw err;
             res.send(docs);    
         })
-    //db.close();
     });
-    
 });
  
 app.get('/', function (req, res) {
